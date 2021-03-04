8bd69f06b1c5082f617f4cad886654288df9dc93
Kévin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2021-03-04 19:57:37 +0100

    Add register and validation work !

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -81,10 +83,19 @@ class User implements UserInterface
      */
     private $discussions;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
+
     public function __construct()
     {
         $this->tricks = new ArrayCollection();
         $this->discussions = new ArrayCollection();
+        $this->created_date = new \DateTime();
+        $this->updated_date = new \DateTime();
+        $this->picture_link = "";
+        $this->email_verified = false;
     }
 
     public function getId(): ?int
@@ -308,6 +319,18 @@ class User implements UserInterface
                 $discussion->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
