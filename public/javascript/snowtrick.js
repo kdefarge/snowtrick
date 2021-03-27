ac3adaab0c3201b42e7b374fc24b7759d9ecc13e
Kévin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2021-03-27 13:42:44 +0100

    Javascript & template : [JS] show only 5 discussion messages [JS] Load more button to show more message template margin correction

--- public/javascript/snowtrick.js
+++ public/javascript/snowtrick.js
@@ -27,6 +27,28 @@ jQuery(document).ready(function() {
         // add a new tag form (see next code block)
         addFormToCollection($collectionHolderClass);
     })
+
+    var discussionMax = 5;
+    var $messageCollection = $('#discussions .discussion-message');
+    if($messageCollection.length > discussionMax) {
+        var currentLoaded = discussionMax;
+        for (var i = discussionMax; i < $messageCollection.length; i++) {
+            $($messageCollection.get(i)).hide();
+        }
+        var $extendButton = $('<button class="btn btn-primary btn-lg active" type="button">Charger plus</button>');
+        $extendButton.on('click', function(e) {
+            for (var i = currentLoaded; i < currentLoaded+discussionMax; i++) {
+                current = $messageCollection.get(i);
+                if(!current)
+                    break;
+                $(current).show();
+            }
+            currentLoaded+=discussionMax;
+            if(currentLoaded>=$messageCollection.length)
+                this.remove();
+        });
+        $('#discussions').append($extendButton);
+    }
 });
 
 function addFormToCollection($collectionHolderClass) {
